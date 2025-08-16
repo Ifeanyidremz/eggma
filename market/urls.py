095433fbf9862e53d1d91c76abdcc883634b4baa
Ifeanyidremz <ifeanyidremz@gmail.com>
2025-08-16 10:54:05 +0100

    Update urls.py

--- market/urls.py
+++ market/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('market/', views.marketPage, name="market-data"),
+    path('', views.marketPage, name="market-data"),
 ]
