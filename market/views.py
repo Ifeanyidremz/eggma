0a3fd82e92aabbdfcd786d598a1add1ecd92cbbc
Ifeanyidremz <ifeanyidremz@gmail.com>
2025-08-20 22:13:46 +0100

    Update views.py

--- market/views.py
+++ market/views.py
@@ -6,3 +6,17 @@ from django.shortcuts import render,HttpResponse
 def marketPage(request):
 
     return render(request, 'market_list.html')
+
+
+
+
+def marketDetail(request):
+
+    return render(request,'market_detail.html')
+
+
+
+
+def userPortfolio(request):
+
+    return render(request, 'portfolio.html')
