8e71eb57a609583736bffb4676303bbcb9ac7ff0
Isaac Abiodun <engrisaac1234@gmail.com>
2026-01-06 12:32:27 +0100

    added few chnages

--- acounts/models.py
+++ acounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 import random
+import string
 from django.utils import timezone
 from decimal import Decimal
 from django.core.validators import MinValueValidator
@@ -110,10 +111,10 @@ def generate_referral_code():
             return code
 
 class ReferralProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='referral_profile')
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='referral_profile')
     referral_code = models.CharField(max_length=20, unique=True, default=generate_referral_code)
     referred_by = models.ForeignKey(
-        User, 
+        CustomUser, 
         on_delete=models.SET_NULL, 
         null=True, 
         blank=True, 
@@ -128,8 +129,8 @@ class ReferralProfile(models.Model):
         return f"{self.user.username} - {self.referral_code}"
 
 class ReferralTransaction(models.Model):
-    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referrer_transactions')
-    referred = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referred_transactions')
+    referrer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='referrer_transactions')
+    referred = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='referred_transactions')
     amount = models.DecimalField(max_digits=20, decimal_places=6)
     transaction_type = models.CharField(max_length=20, choices=[
         ('signup', 'Signup Bonus'),
